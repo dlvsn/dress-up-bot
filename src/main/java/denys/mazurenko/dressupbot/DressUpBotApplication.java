ec6530f4048d263f5762faba696025702598e5fe
Denys Mazurenko <0gur4ik3992@gmail.com>
2025-05-29 12:50:08 +0300

    Add project infrastructure

---
+++ src/main/java/denys/mazurenko/dressupbot/DressUpBotApplication.java
@@ -0,0 +1,11 @@
+package denys.mazurenko.dressupbot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DressUpBotApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(DressUpBotApplication.class, args);
+    }
+}
---
+++ src/test/java/denys/mazurenko/dressupbot/DressUpBotApplicationTests.java
@@ -0,0 +1,13 @@
+package denys.mazurenko.dressupbot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DressUpBotApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
